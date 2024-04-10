3305f441982a3b3f42bc8d86598eff0e34cf0ffd
AshwinHub <phantomfills@outlook.com>
2024-04-10 20:39:17 +0100

    🔨introduce hoarcekat story

---
+++ src/client/stories/component/board.story.tsx
@@ -0,0 +1,7 @@
+import React from "@rbxts/react";
+import { hoarcekat } from "@rbxts/pretty-react-hooks";
+import { Board } from "client/components/board";
+
+export = hoarcekat(() => {
+	return <Board />;
+});
