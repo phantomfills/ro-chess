c0f3d8070db2f4e72e2596211989991419501753
AshwinHub <phantomfills@outlook.com>
2024-04-12 15:56:02 +0100

    🔨introduce all black piece movement

--- src/shared/store/board/board-selectors.ts
+++ src/shared/store/board/board-selectors.ts
@@ -1,4 +1,4 @@
-import { getPieceIsBlack } from "shared/constants/piece";
+import { getPieceIsBlack, getPieceIsWhite } from "shared/constants/piece";
 import { SharedState } from "..";
 import { numberRange } from "shared/utils/math-utils";
 
@@ -10,7 +10,14 @@ export function selectPieceAtCell(cell: number) {
 	return (state: SharedState) => state.board.cells[cell];
 }
 
-function getLinearMoves(state: SharedState, startRow: number, startCol: number, rowOffset: number, colOffset: number) {
+function getLinearMoves(
+	state: SharedState,
+	startRow: number,
+	startCol: number,
+	rowOffset: number,
+	colOffset: number,
+	isPieceBlack: boolean,
+) {
 	const moves = [];
 
 	for (let offset = 1; offset < 8; offset++) {
@@ -25,7 +32,7 @@ function getLinearMoves(state: SharedState, startRow: number, startCol: number, 
 		const targetPiece = state.board.cells[move];
 
 		if (targetPiece !== false) {
-			if (getPieceIsBlack(targetPiece)) {
+			if (isPieceBlack ? !getPieceIsBlack(targetPiece) : getPieceIsBlack(targetPiece)) {
 				moves.push({ row, col });
 			}
 			break;
@@ -37,7 +44,7 @@ function getLinearMoves(state: SharedState, startRow: number, startCol: number, 
 	return moves;
 }
 
-function getKingMoves(state: SharedState, startRow: number, startCol: number) {
+function getKingMoves(state: SharedState, startRow: number, startCol: number, isPieceBlack: boolean) {
 	const directions = [
 		{ row: -1, col: -1 },
 		{ row: -1, col: 0 },
@@ -62,7 +69,7 @@ function getKingMoves(state: SharedState, startRow: number, startCol: number) {
 		const move = row * 8 + col;
 		const targetPiece = state.board.cells[move];
 
-		if (targetPiece !== false && !getPieceIsBlack(targetPiece)) {
+		if (targetPiece !== false && (isPieceBlack ? !getPieceIsWhite(targetPiece) : !getPieceIsBlack(targetPiece))) {
 			continue;
 		}
 
@@ -148,34 +155,34 @@ export function selectLegalMovesForCell(cell: number) {
 			}
 			case "white-rook": {
 				return [
-					...getLinearMoves(state, cellRow, cellCol, 0, 1),
-					...getLinearMoves(state, cellRow, cellCol, 0, -1),
-					...getLinearMoves(state, cellRow, cellCol, 1, 0),
-					...getLinearMoves(state, cellRow, cellCol, -1, 0),
+					...getLinearMoves(state, cellRow, cellCol, 0, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, 0, -1, false),
+					...getLinearMoves(state, cellRow, cellCol, 1, 0, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, 0, false),
 				].map(({ row, col }) => row * 8 + col);
 			}
 			case "white-bishop": {
 				return [
-					...getLinearMoves(state, cellRow, cellCol, 1, 1),
-					...getLinearMoves(state, cellRow, cellCol, 1, -1),
-					...getLinearMoves(state, cellRow, cellCol, -1, 1),
-					...getLinearMoves(state, cellRow, cellCol, -1, -1),
+					...getLinearMoves(state, cellRow, cellCol, 1, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, 1, -1, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, -1, false),
 				].map(({ row, col }) => row * 8 + col);
 			}
 			case "white-queen": {
 				return [
-					...getLinearMoves(state, cellRow, cellCol, 0, 1),
-					...getLinearMoves(state, cellRow, cellCol, 0, -1),
-					...getLinearMoves(state, cellRow, cellCol, 1, 0),
-					...getLinearMoves(state, cellRow, cellCol, -1, 0),
-					...getLinearMoves(state, cellRow, cellCol, 1, 1),
-					...getLinearMoves(state, cellRow, cellCol, 1, -1),
-					...getLinearMoves(state, cellRow, cellCol, -1, 1),
-					...getLinearMoves(state, cellRow, cellCol, -1, -1),
+					...getLinearMoves(state, cellRow, cellCol, 0, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, 0, -1, false),
+					...getLinearMoves(state, cellRow, cellCol, 1, 0, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, 0, false),
+					...getLinearMoves(state, cellRow, cellCol, 1, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, 1, -1, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, 1, false),
+					...getLinearMoves(state, cellRow, cellCol, -1, -1, false),
 				].map(({ row, col }) => row * 8 + col);
 			}
 			case "white-king": {
-				return getKingMoves(state, cellRow, cellCol).map(({ row, col }) => row * 8 + col);
+				return getKingMoves(state, cellRow, cellCol, false).map(({ row, col }) => row * 8 + col);
 			}
 			case "black-pawn": {
 				const attackingMoves = [cell - 7, cell - 9].filter((move) => {
@@ -210,6 +217,67 @@ export function selectLegalMovesForCell(cell: number) {
 				const movesInBounds = potentialmoves.filter((move) => move >= 0 && move < 64);
 
 				return movesInBounds;
+			}
+			case "black-knight": {
+				const moves = [
+					{ row: -2, col: -1 },
+					{ row: -2, col: 1 },
+					{ row: -1, col: -2 },
+					{ row: -1, col: 2 },
+					{ row: 1, col: -2 },
+					{ row: 1, col: 2 },
+					{ row: 2, col: -1 },
+					{ row: 2, col: 1 },
+				].map(({ row, col }) => {
+					const cellRow = math.floor(cell / 8);
+					const cellCol = cell % 8;
+					return { row: cellRow + row, col: cellCol + col };
+				});
+
+				const legalMoves = moves.filter(({ row, col }) => {
+					if (row < 0 || row > 7 || col < 0 || col > 7) {
+						return false;
+					}
+
+					const move = row * 8 + col;
+					const targetPiece = state.board.cells[move];
+
+					const pieceBlocks = targetPiece !== false && !getPieceIsWhite(targetPiece);
+					return !pieceBlocks;
+				});
+
+				return legalMoves.map(({ row, col }) => row * 8 + col);
+			}
+			case "black-rook": {
+				return [
+					...getLinearMoves(state, cellRow, cellCol, 0, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, 0, -1, true),
+					...getLinearMoves(state, cellRow, cellCol, 1, 0, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, 0, true),
+				].map(({ row, col }) => row * 8 + col);
+			}
+			case "black-bishop": {
+				return [
+					...getLinearMoves(state, cellRow, cellCol, 1, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, 1, -1, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, -1, true),
+				].map(({ row, col }) => row * 8 + col);
+			}
+			case "black-queen": {
+				return [
+					...getLinearMoves(state, cellRow, cellCol, 0, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, 0, -1, true),
+					...getLinearMoves(state, cellRow, cellCol, 1, 0, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, 0, true),
+					...getLinearMoves(state, cellRow, cellCol, 1, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, 1, -1, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, 1, true),
+					...getLinearMoves(state, cellRow, cellCol, -1, -1, true),
+				].map(({ row, col }) => row * 8 + col);
+			}
+			case "black-king": {
+				return getKingMoves(state, cellRow, cellCol, true).map(({ row, col }) => row * 8 + col);
 			}
 			default: {
 				return [];
